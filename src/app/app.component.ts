9720414481dc7edf8927df1fb43bcd781df9913a
Alishbaiftikhar <alishbaiftikhar2001@gmail.com>
2024-02-23 04:16:23 -0800

    adding video upload in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {VideoUploadComponent} from './video-upload/video-upload.component'
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, VideoUploadComponent]
 })
 export class AppComponent {
   title = 'VideoApplication';
---
+++ src/app/video-upload/video-upload.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-upload',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      video-upload works!
+    </p>
+  `,
+  styleUrl: './video-upload.component.css'
+})
+export class VideoUploadComponent {
+
+}
